fa42772ec3613c6d4ab3e8c19bfa96fd5fc5f0b2
Zachary Boelter <zboelter@gmail.com>
2022-03-01 13:03:09 -0600

    return leads

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const inputBtn = document.querySelector('#input-btn');
 const inputEl = document.querySelector('#input-el');
 
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     console.log(myLeads);
 })
 
+function printLeads() {
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i]);
+    }
+}
+
+printLeads();
