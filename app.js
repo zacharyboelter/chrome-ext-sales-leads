3c2ee03d6815de5b35d0d83e22acece364863614
Zachary Boelter <zboelter@gmail.com>
2022-03-01 14:45:59 -0600

    clear input field, append input to ul

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+//Dom Elements
 const inputBtn = document.querySelector('#input-btn');
 const inputEl = document.querySelector('#input-el');
+const ulEl = document.querySelector('#ul-el');
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 
 
+// Click button push to array
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     console.log(myLeads);
+    renderLeads();
+    
 })
 
-function printLeads() {
-    for (let i = 0; i < myLeads.length; i++) {
-        console.log(myLeads[i]);
-    }
-}
 
-printLeads();
+// return and display array values
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li>${myLeads[i]}</li>`;
+    }
+    ulEl.innerHTML = listItems;
+    inputEl.value = '';
+}
