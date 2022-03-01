efa6956151f8753349f6525ea9b43ec443fda5a0
Zachary Boelter <zboelter@gmail.com>
2022-03-01 12:34:49 -0600

    event listener

---
+++ app.js
@@ -0,0 +1,8 @@
+let inputBtn = document.querySelector('#input-btn');
+let inputEl = document.querySelector('#input-el');
+
+
+inputBtn.addEventListener("click", function() {
+    console.log('Button clicked from addEventLisstener')
+})
+
