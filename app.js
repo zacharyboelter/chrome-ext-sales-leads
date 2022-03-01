cbd886a669205b263f01a51ccb3914bbb05d7934
Zachary Boelter <zboelter@gmail.com>
2022-03-01 15:06:38 -0600

    style list

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let myLeads = [];
 
 // Click button push to array
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
+    myLeads.unshift(inputEl.value);
     console.log(myLeads);
     inputEl.value = '';
     renderLeads();
@@ -19,11 +19,13 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems +=`<li>
-                        <a href=${myLeads[i]} target="_blank">
+        listItems +=`
+                    <li>
+                        <a href='${myLeads[i]}' target="_blank">
                             ${myLeads[i]}
                         </a>
-                     </li>`;
+                     </li>
+    `;
     }
     ulEl.innerHTML = listItems;
 }
